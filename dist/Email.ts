e54ef7ec2438b15d020faf22280292587d13e982
Curt Weber <cmkweber@gmail.com>
2022-09-15 13:05:52 -0500

    Typo

--- dist/Email.ts
+++ dist/Email.ts
@@ -5,5 +5,5 @@ import {JsonString} from 'json';
 export class JsonEmail extends JsonString
 {
 	// Email constructor
-	constructor(value?:string) { super(new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/), value !== undefined ? value : 'user@email.com');  }
+	constructor(value?:string) { super(new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/), value !== undefined ? value : 'user@email.com'); }
 }
